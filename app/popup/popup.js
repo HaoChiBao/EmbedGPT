5f1e50b6bc497411e1a857a93005c0d339c882f8
James Yang <jamesyang663@gmail.com>
2024-05-28 22:33:09 -0400

    styled login and register pages

--- app/popup/popup.js
+++ app/popup/popup.js
@@ -2,7 +2,8 @@
 const start_btn = document.querySelector('button');
 // redirect to chat page
 // window.location.href = "pages/chat/chat.html";
+window.location.href = "pages/register/register.html";
 
 start_btn.addEventListener('click', () => {
-    window.location.href = "pages/chat/chat.html";
+    window.location.href = "pages/register/register.html";
 });
