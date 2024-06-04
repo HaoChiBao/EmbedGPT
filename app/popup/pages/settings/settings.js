846d2a1c53f73a822ecc11a060003b91a5501f65
James Yang <jamesyang663@gmail.com>
2024-06-03 20:30:50 -0400

    settings added (email shown on screen

--- app/popup/pages/settings/settings.js
+++ app/popup/pages/settings/settings.js
@@ -22,14 +22,42 @@ port.onMessage.addListener((msg) => {
 
 const main = async () => {
     const redirectChat = document.getElementById('redirect-chat')
-    redirectChat.addEventListener('click', () => {
-        window.location.href = "../chat/chat.html";
-    })
+    
+    const email = document.getElementById('email')
+    const emailText = email.querySelector('p')
 
-    const logout = document.getElementById('logout')    
-    logout.addEventListener('click', () => {
-        port.postMessage({ action: 'signOut' })
-    })
+    const plan = document.getElementById('plan')
+    const planText = plan.querySelector('p')
+
+    const archive = document.getElementById('archive')
+
+    const theme = document.getElementById('color-theme')
+    const themeText = theme.querySelector('p')
+    
+    const deleteChats = document.getElementById('delete-chats')
+    const logout = document.getElementById('logout')
+
+    // get user credentials
+    const userCredentials = await chrome.storage.local.get('w_userCredentials')
+    if(!userCredentials.w_userCredentials) {
+        console.log('User is not logged in')
+        window.location.href = "../../popup.html";
+        return
+    }
+
+    emailText.innerText = userCredentials.w_userCredentials.email
+
+    email.addEventListener('click', () => {console.log('email');})
+    plan.addEventListener('click', () => {console.log('plan');})
+    archive.addEventListener('click', () => {console.log('archive');})
+    theme.addEventListener('click', () => {console.log('theme');})
+    deleteChats.addEventListener('click', () => {console.log('deleteChats');})
+
+    // return user to chat page
+    redirectChat.addEventListener('click', () => {window.location.href = "../chat/chat.html";})
+
+    // logout user
+    logout.addEventListener('click', () => {port.postMessage({ action: 'signOut' })})
 }
 main()
 
